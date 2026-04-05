7c357f0702cec1c096364f8e9cb3859dd39fc5f9
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-04-05 13:13:45 +0900

    Seems to be kind working now

--- Formula/brb.rb
+++ Formula/brb.rb
@@ -4,17 +4,16 @@ class Brb < Formula
   license ""
 
   version "1.0.0"
-  url "https://github.com/rafalgrodzinski/bits-runner-builder/archive/refs/tags/dev/1.0.0-dev-91.tar.gz"
-  sha256 "09b320d4ebce8929a54408d39a32578f5799393d24dec992930f89823c904ece"
-
+  url "https://github.com/rafalgrodzinski/bits-runner-builder/archive/refs/heads/129-automated-cmake-for-build-installation-on-macos.zip"
+  sha256 "4ccb1d0f82f114a3975a319af4ead5aac76f5d2d37f8a1e8c51c97f03f460027"
+  
   depends_on "cmake" => :build
   depends_on "llvm@20"
   depends_on macos: :sonoma
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 end
