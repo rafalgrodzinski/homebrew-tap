9cdaafc5d28af4729282b3521766a810aa07f0e6
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-04-05 13:59:06 +0900

    Fixed styling

--- Formula/brb.rb
+++ Formula/brb.rb
@@ -3,12 +3,12 @@ class Brb < Formula
   homepage "https://github.com/rafalgrodzinski/bits-runner-builder"
   license ""
 
-  version "1.0.0-dev-92"
   url "https://github.com/rafalgrodzinski/bits-runner-builder/archive/refs/tags/dev/1.0.0-dev-92.tar.gz"
   sha256 "ba657f1836a48a4e77e45d62ac8ddf7b55b395187df6a26c8958e52aaf1effb7"
-  
+  version "1.0.0-dev-92"
+
   head "https://github.com/rafalgrodzinski/bits-runner-builder.git", branch: "main"
-  
+
   depends_on "cmake" => :build
   depends_on "llvm@20"
   depends_on macos: :sonoma
