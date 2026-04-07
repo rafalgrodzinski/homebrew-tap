19dad443c0aae1a871d9a65f0ce61d549fed4438
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-04-07 16:10:33 +0900

    Updated build

--- Formula/brb.rb
+++ Formula/brb.rb
@@ -1,11 +1,15 @@
 class Brb < Formula
+  BUILD_KIND = "dev".freeze
+  VERSION = "1.0.0-dev-95".freeze
+  SHA_256 = "aaad4257b57724c17b5dfa9118dcdc7b6ee110434d2976b341235ee6a1a26eda".freeze
+
   desc "Compiler for the Bits Runner Code language"
   homepage "https://github.com/rafalgrodzinski/bits-runner-builder"
-  license ""
 
-  url "https://github.com/rafalgrodzinski/bits-runner-builder/archive/refs/tags/dev/1.0.0-dev-92.tar.gz"
-  sha256 "ba657f1836a48a4e77e45d62ac8ddf7b55b395187df6a26c8958e52aaf1effb7"
-  version "1.0.0-dev-92"
+  url "https://github.com/rafalgrodzinski/bits-runner-builder/archive/refs/tags/#{BUILD_KIND}/#{VERSION}.tar.gz"
+  version VERSION
+  sha256 SHA_256
+  license ""
 
   head "https://github.com/rafalgrodzinski/bits-runner-builder.git", branch: "main"
 
@@ -14,7 +18,7 @@ class Brb < Formula
   depends_on macos: :sonoma
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "VERSION=#{VERSION}", "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
