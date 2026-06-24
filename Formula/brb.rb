58c5acedb7bac9808e3c5216c600bfe4e80ed1eb
Rafał <rafal.grodzinski@gmail.com>
2026-06-24 14:33:50 +0900

    Updated to version 1.0.0-dev-104

--- Formula/brb.rb
+++ Formula/brb.rb
@@ -1,7 +1,7 @@
 class Brb < Formula
   BUILD_KIND = "dev".freeze
-  VERSION = "1.0.0-dev-102".freeze
-  SHA_256 = "8986bac0b081f51e73eae2b1b11630de53a53261c0eb402dcd5a0bd2d6764069".freeze
+  VERSION = "1.0.0-dev-104".freeze
+  SHA_256 = "7b3e79c99ac32bb426ab47672bdb25d5365f5e9da7db1bd53c6746a5c2bcb104".freeze
 
   desc "Compiler for the Bits Runner Code language"
   homepage "https://github.com/rafalgrodzinski/bits-runner-builder"
